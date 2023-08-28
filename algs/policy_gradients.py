Fixed the numerical blow up with simple gradient clipping

algs/policy_gradients.py
@@ -4,6 +4,7 @@ from jax.numpy import linalg as jla
 import jax.nn as nn
 
 from itertools import accumulate
+from tqdm import tqdm
 
 from env.mdp import MarkovDecisionProcess, Sampler
 
@@ -11,18 +12,24 @@ from env.mdp import MarkovDecisionProcess, Sampler
 def flatten(v):
     return jnp.reshape(v,(list(accumulate(v.shape,lambda x,y:x*y))[-1],))
 
+CLIP_THRESH = 1e3
+
 class PolicyGradientMethod():
-    def __init__(self, MDP, key, gradient_sampler, logger) -> None:
+    def __init__(self, MDP, key, gradient_sampler, logger, progress_bar=True, clip_thresh=CLIP_THRESH) -> None:
         self.MDP : MarkovDecisionProcess = MDP
         self.key = key
         self.gradient_sampler = gradient_sampler
         self.logger = logger
+        self.progress_bar = progress_bar
+        self.clip_thresh = clip_thresh
         
     def train(self, nb_steps, lr ):
         theta = jax.random.uniform(self.key,(self.MDP.n,self.MDP.m))
         log = [self.logger(theta)]
-        for _ in range(nb_steps):
-            theta += lr * self.gradient_sampler(theta)
+        for _ in tqdm(range(nb_steps),disable=not self.progress_bar):
+            theta += lr * jnp.clip(self.gradient_sampler(theta),
+                                   a_min=-self.clip_thresh,
+                                   a_max=self.clip_thresh)
             log += [self.logger(theta)]
         return log, theta
 
@@ -75,3 +82,26 @@ def computeMonteCarloVanillaGrad(theta,mdp,sampler,key,parametrization,B,H):
 
 def monteCarloVanillaGrad(mdp,sampler,key,parametrization,B,H):
     return lambda p : computeMonteCarloVanillaGrad(p,mdp,sampler,key,parametrization,B,H)
+
+"""Stochastic natural gradients and FIM estimation"""
+
+def fim_sample(sa,theta):
+    pg = flatten(jax.grad(lambda p : jnp.log(nn.softmax(p,axis=1))[sa[0],sa[1]])(theta))
+    return jnp.outer(pg,pg)
+def estimate_fim(batch,theta):
+    sa_pairs = jnp.array([flatten(jnp.array([[step[0] for step in trace] for trace in batch])), flatten(jnp.array([[step[1] for step in trace] for trace in batch]))])
+    return jnp.sum(jnp.array([fim_sample(sa_pairs[:,i],theta) for i in range(sa_pairs.shape[1])]),axis=0)/sa_pairs.shape[1]
+
+
+def truncate_parameters(theta):
+    return 
+
+def computeMonteCarloNaturalGrad(theta,mdp,sampler,key,parametrization,B,H):
+    batch = sample_batch(parametrization(theta),mdp,sampler,H,B,key)
+    _g = gpomdp(batch,theta,B,H,mdp.gamma)
+    _shape = _g.shape
+    fim = estimate_fim(batch,theta)
+    return jnp.reshape(jla.pinv(fim)@flatten(_g),_shape) # TODO : replace with conjugate grads
+
+def monteCarloNaturalGrad(mdp,sampler,key,parametrization,B,H):
+    return lambda p : computeMonteCarloNaturalGrad(p,mdp,sampler,key,parametrization,B,H)

run_experiment.py
@@ -11,7 +11,7 @@ from itertools import accumulate
 
 from env.gridworld import Gridworld
 
-from algs.policy_gradients import PolicyGradientMethod, sample_trajectory, vanillaGradOracle, naturalGradOracle, monteCarloVanillaGrad, Sampler
+from algs.policy_gradients import PolicyGradientMethod, vanillaGradOracle, naturalGradOracle, monteCarloVanillaGrad, Sampler, monteCarloNaturalGrad
 
 def flatten(v):
     return jnp.reshape(v,(list(accumulate(v.shape,lambda x,y:x*y))[-1],))
@@ -27,7 +27,7 @@ gridMDP.init_distrib =  jnp.exp(jax.random.uniform(key,(gridMDP.n,))) / \
 
 parametrization = lambda p : nn.softmax(p,axis=1)
 
-HORIZON = 10
+HORIZON = 5
 BATCH = 10
 sampler = Sampler(gridMDP,key)
 
@@ -39,6 +39,7 @@ sampler = Sampler(gridMDP,key)
 """Defining the logger"""
 def logger(theta):
         return {
+            'theta': theta,
             'pi': nn.softmax(theta,axis=1),
             'J': gridMDP.J(nn.softmax(theta,axis=1))
         }
@@ -46,11 +47,17 @@ def logger(theta):
 
 """Trainig"""
 alg = PolicyGradientMethod(gridMDP,key,
-                           monteCarloVanillaGrad(gridMDP,sampler,key,parametrization,HORIZON,BATCH)
+                           monteCarloNaturalGrad(
+                               gridMDP,
+                               sampler,
+                               key,
+                               parametrization,
+                               HORIZON,BATCH)
                            ,logger)
-log, theta = alg.train(10,4e-2)
+log, theta = alg.train(20,4e-2)
 
 """Plotting the training curve"""
+thetas=jnp.stack([e['theta'] for e in log])
 pis=jnp.stack([e['pi'] for e in log])
 js=jnp.stack([e['J'] for e in log])
 fig, ax = plt.subplots(1,2,figsize=(10,5))