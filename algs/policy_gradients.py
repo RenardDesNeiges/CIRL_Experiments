3cd1ebcd22c5bd11d38fd833b769be3238216ab6
Titouan Renard <titouan.renard@gmail.com>
2023-08-28 09:07:43 +0200

    Small refactor

--- algs/policy_gradients.py
+++ algs/policy_gradients.py
@@ -1,27 +1,77 @@
 import jax
 from jax import numpy as jnp
 from jax.numpy import linalg as jla
-from einops import rearrange, repeat
+import jax.nn as nn
+
 from itertools import accumulate
 
+from env.mdp import MarkovDecisionProcess, Sampler
 
+""" Useful helper functions """
+def flatten(v):
+    return jnp.reshape(v,(list(accumulate(v.shape,lambda x,y:x*y))[-1],))
 
+class PolicyGradientMethod():
+    def __init__(self, MDP, key, gradient_sampler, logger) -> None:
+        self.MDP : MarkovDecisionProcess = MDP
+        self.key = key
+        self.gradient_sampler = gradient_sampler
+        self.logger = logger
+        
+    def train(self, nb_steps, lr ):
+        theta = jax.random.uniform(self.key,(self.MDP.n,self.MDP.m))
+        log = [self.logger(theta)]
+        for _ in range(nb_steps):
+            theta += lr * self.gradient_sampler(theta)
+            log += [self.logger(theta)]
+        return log, theta
 
+""" Generic sampling routines for MDPs """
+def sample_trajectory(pi,mdp,smp,H,key):
+    def pick_action(pi,s,mdp):
+        p = pi[s]; p /= jnp.sum(p)
+        return jax.random.choice(key,jnp.arange(mdp.m), p = p)
+        
+    traj = []
+    r_t = 0
+    s_t = smp.reset()
+    for _ in range(H):
+        a_t = pick_action(pi,s_t,mdp)
+        traj += [(s_t,a_t,r_t)]
+        s_t, r_t = smp.step(a_t)
+    return traj
+def sample_batch(pi,mdp,smp,H,B,key):
+    return [sample_trajectory(pi,mdp,smp,H,key) for _ in range(B)]
 
-## ==> Moved to the mdp class
-# def closed_loop_kernel(mdp,pi):
-#     return jnp.einsum('sap,sa->sp',mdp.P_sa,pi) # closed loop transition kernel
+"""Exact gradient oracles"""
 
-# def state_occ_measure(mdp,P_pi):
-#     return (1-mdp.gamma)*(jla.inv((jnp.eye(mdp.n)-mdp.gamma*P_pi)).transpose()@mdp.init_distrib)
-# def occ_measure(mdp,pi):
-#     mu_s = state_occ_measure(mdp,closed_loop_kernel(mdp,pi))
-#     return pi * repeat(mu_s, 's -> s new_axis', new_axis=mdp.m)
-# def J_unregularized(mdp,theta):
-#     return jnp.dot(flatten(mdp.R),flatten(occ_measure(mdp,softmax_pi(theta))))
+def vanillaGradOracle(mdp,sampler,key,parametrization,B,H):
+    return jax.grad(lambda p : mdp.J(parametrization(p)))
 
-def exact_fim_oracle(theta,parametrization,mdp):
-    v = jax.jacfwd(lambda p : flatten(jnp.log(softmax_pi(p))))(theta) # computing the jacobian (step 1)
-    jac = jnp.reshape(v,(mdp.n*mdp.m,mdp.n*mdp.m)) # flatten last two dimensions (step 1)
-    bop = jnp.einsum('bi,bj->bji',jac,jac) # batch outer-product (step 2)
-    return jnp.einsum('bij,b->ij',bop,flatten(occ_measure(mdp,softmax_pi(theta)))) # fisher information matrix (I hope) (step 3)aa
+def naturalGradOracle(mdp,sampler,key,parametrization,B,H):
+    def naturalGrad(theta):
+        _shape = theta.shape
+        g = jax.grad(lambda p : mdp.J(parametrization(p)))(theta)    
+        f_inv = jla.pinv(mdp.exact_fim_oracle(theta,lambda p:nn.softmax(p,axis=1)))
+        return jnp.reshape(f_inv@flatten(g),_shape)
+    return naturalGrad
+
+"""Stochastic gradients"""
+
+def gpomdp(batch,theta,B,H,gamma):
+    def g_log(theta,s,a):
+        return jax.grad(lambda p : jnp.log(nn.softmax(p,axis=1))[s,a])(theta)
+    def trace_grad(batch,theta,b,h):
+        return jnp.sum(jnp.array([g_log(theta,e[0],e[1]) for e in batch[b][:h]]),axis=0)
+    def single_sample_gpomdp(batch,theta,b,H):
+        return jnp.sum(jnp.array([(gamma**h)*batch[b][h][2] \
+                    *trace_grad(batch,theta,b,h) for h in range(1,H)]),axis=0)
+        
+    return (1/B)*jnp.sum(jnp.array([single_sample_gpomdp(batch,theta,b,H) for b in range(B)]),axis=0)
+
+def computeMonteCarloVanillaGrad(theta,mdp,sampler,key,parametrization,B,H):
+    batch = sample_batch(parametrization(theta),mdp,sampler,H,B,key)
+    return gpomdp(batch,theta,B,H,mdp.gamma)
+
+def monteCarloVanillaGrad(mdp,sampler,key,parametrization,B,H):
+    return lambda p : computeMonteCarloVanillaGrad(p,mdp,sampler,key,parametrization,B,H)
--- run_experiment.py
+++ run_experiment.py
@@ -4,11 +4,14 @@ from jax import numpy as jnp
 from jax.numpy import linalg as jla
 import jax.nn as nn
 
+from jax.config import config; config.update("jax_enable_x64", True)
+
 import matplotlib.pyplot as plt
 from itertools import accumulate
 
-from env.mdp import MarkovDecisionProcess,Sampler
-from env.gridworld import Gridworld, gridplot
+from env.gridworld import Gridworld
+
+from algs.policy_gradients import PolicyGradientMethod, sample_trajectory, vanillaGradOracle, naturalGradOracle, monteCarloVanillaGrad, Sampler
 
 def flatten(v):
     return jnp.reshape(v,(list(accumulate(v.shape,lambda x,y:x*y))[-1],))
@@ -16,42 +19,38 @@ def flatten(v):
 key = jax.random.PRNGKey(0) 
 
 """Defining an MDP"""
-goals = [((2,2),100)]
-gridMDP = Gridworld(3,3,0.1,0.9,goals=goals,obstacles=[]) # this is our MDP
+goals = [((1,1),100)]
+gridMDP = Gridworld(2,2,0.1,0.9,goals=goals,obstacles=[]) # this is our MDP
 gridMDP.init_distrib =  jnp.exp(jax.random.uniform(key,(gridMDP.n,))) / \
     jnp.sum(jnp.exp(jax.random.uniform(key,(gridMDP.n,)))) # setting its init distribution
-    
-    
-"""Plotting the MDP"""
-
-# fig, ax = plt.subplots(1,figsize=(10,10))
-# gridplot(gridMDP,ax,goals=goals)
-# ax.set_title('Gridworld reward structure')
-# fig.tight_layout()
-# plt.show()
-
-vanillaGrad = jax.grad(lambda p : gridMDP.J(nn.softmax(p,axis=1)))
-def naturalGrad(theta):
-    _shape = theta.shape
-    g = jax.grad(lambda p : gridMDP.J(nn.softmax(p,axis=1)))(theta)    
-    f_inv = jla.pinv(gridMDP.exact_fim_oracle(theta,lambda p:nn.softmax(p,axis=1)))
-    return jnp.reshape(f_inv@flatten(g),_shape)
 
 
-def PolicyGradient(_g,mdp,eta=20, nb_steps = 10):
-    def logger(theta):
+parametrization = lambda p : nn.softmax(p,axis=1)
+
+HORIZON = 10
+BATCH = 10
+sampler = Sampler(gridMDP,key)
+
+""" Picking the gradient evaluation method 
+    (either through an orcale or through sampling)
+    (either preconditionned with the inverse FIM or not)
+"""
+
+"""Defining the logger"""
+def logger(theta):
         return {
             'pi': nn.softmax(theta,axis=1),
-            'J': mdp.J(nn.softmax(theta,axis=1))
+            'J': gridMDP.J(nn.softmax(theta,axis=1))
         }
-    theta = jax.random.uniform(key,(mdp.n,mdp.m))
-    log = [logger(theta)]
-    for _ in range(nb_steps):
-        theta += eta * _g(theta)
-        log += [logger(theta)]
-    return log, theta
-log, theta = PolicyGradient(naturalGrad,gridMDP,5e-2)
 
+
+"""Trainig"""
+alg = PolicyGradientMethod(gridMDP,key,
+                           monteCarloVanillaGrad(gridMDP,sampler,key,parametrization,HORIZON,BATCH)
+                           ,logger)
+log, theta = alg.train(10,4e-2)
+
+"""Plotting the training curve"""
 pis=jnp.stack([e['pi'] for e in log])
 js=jnp.stack([e['J'] for e in log])
 fig, ax = plt.subplots(1,2,figsize=(10,5))
