a4b9d867abfda78e2f346f9bbf87151b7bb6470c
Titouan Renard <titouan.renard@gmail.com>
2023-09-05 10:39:18 +0200

    Example MDP libary

--- algs/utils.py
+++ algs/utils.py
@@ -1,9 +1,67 @@
 import jax
 from jax import numpy as jnp
 from itertools import accumulate
+from abc import ABC, abstractmethod
+
+from env.mdp import MarkovDecisionProcess
+from env.complete import Complete
+from env.gridworld import Gridworld
+from algs.projs import euclidean_l2ball
 
 """ Useful helper functions """
 def flatten(v):
     return jnp.reshape(v,(list(accumulate(v.shape,lambda x,y:x*y))[-1],))
 
 shannonEntropy = lambda p : jnp.sum(jax.scipy.special.entr(p))
+
+class ExampleMDPs(ABC):
+    @abstractmethod
+    def bandit1(reward_class:str='L2')->MarkovDecisionProcess:
+        key = jax.random.PRNGKey(1)
+        mdp = Complete(1,2,0.95)
+        key, sk = jax.random.split(key)
+        mdp.init_distrib = jnp.exp(jax.random.uniform(sk,(mdp.n,))) / \
+        jnp.sum(jnp.exp(jax.random.uniform(key,(mdp.n,))))
+        assert reward_class == 'L2'
+        mdp.R = euclidean_l2ball(mdp.R, 1)
+        return mdp
+    
+    @abstractmethod
+    def minimal1(reward_class:str='L2')->MarkovDecisionProcess:
+        key = jax.random.PRNGKey(1)
+        mdp = Complete(2,2,0.95)
+        key, sk = jax.random.split(key)
+        mdp.init_distrib = jnp.exp(jax.random.uniform(sk,(mdp.n,))) / \
+        jnp.sum(jnp.exp(jax.random.uniform(key,(mdp.n,))))
+        assert reward_class == 'L2'
+        mdp.R = euclidean_l2ball(mdp.R, 1)
+        return mdp
+    
+    @abstractmethod
+    def gworld1(reward_class:str='L2')->MarkovDecisionProcess:
+        key = jax.random.PRNGKey(1)
+        R = 100; P = -300; 
+        goals = [((2,0),R)]
+        mdp = Gridworld(3,3,0.1,0.9,goals=goals,obstacles=[]) 
+        key, sk = jax.random.split(key)
+        mdp.init_distrib = jnp.exp(jax.random.uniform(sk,(mdp.n,))) / \
+        jnp.sum(jnp.exp(jax.random.uniform(key,(mdp.n,))))
+        assert reward_class == 'L2'
+        mdp.R = euclidean_l2ball(mdp.R, 1)
+        return mdp
+
+    @abstractmethod
+    def gworld2(reward_class:str='L2')->MarkovDecisionProcess:
+        key = jax.random.PRNGKey(1)
+        R = 100; P = -300; 
+        R = 100; P = -300; goals = [((2,0),R),((1,0),P),((1,1),P)]
+        mdp = Gridworld(3,3,0.1,0.9,goals=goals,obstacles=[]) 
+        mdp.init_distrib =  jnp.exp(jax.random.uniform(key,(mdp.n,))) / \
+            jnp.sum(jnp.exp(jax.random.uniform(key,(mdp.n,))))
+        key, sk = jax.random.split(key)
+        mdp.init_distrib = jnp.exp(jax.random.uniform(sk,(mdp.n,))) / \
+        jnp.sum(jnp.exp(jax.random.uniform(key,(mdp.n,))))
+        assert reward_class == 'L2'
+        mdp.R = euclidean_l2ball(mdp.R, 1)
+        return mdp
+    
